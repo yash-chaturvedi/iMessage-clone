8b3f40fe871354b912b1a94b15f30ad13ffa2464
zeref10 <yashchaturvedi296@gmail.com>
2020-11-18 23:03:29 +0530

    completed basic chat section

--- src/Chat.js
+++ src/Chat.js
@@ -1,3 +1,5 @@
+import { IconButton } from '@material-ui/core';
+import MicNoneIcon from '@material-ui/icons/MicNone'
 import React, { useState } from 'react'
 import './Chat.css'
 
@@ -15,14 +17,21 @@ function Chat() {
                 <h4>To : <span className="chat__name">Channel Name</span></h4>
                 <strong>Details</strong>
             </div>
+
+            
             <div className="chat__messages">
 
             </div>
+
+
             <div className="chat__input">
                 <form>
                     <input value={input} onChange = {(e) => setInput(e.target.value)} placeholder="iMessage" type="text"/>
                     <button onClick={sendMessage}>Send Message</button>
                 </form>
+                <IconButton>
+                    <MicNoneIcon className="chat__mic" />
+                </IconButton>
             </div>
         </div>
     )
